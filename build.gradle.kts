9e1c412b2b4f9278dc988edc25f0c8e91987bd40
cjh8487 <ppzxc8487@gmail.com>
2024-02-23 13:54:57 +0900

    2024-02-23 release/0.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,25 @@
 plugins {
-    id("java")
+    java
 }
 
-allprojects {
-    group = "com.github.ppzxc"
-    version = "0.0.3"
+group = "com.github.ppzxc"
+version = "0.0.4"
 
-    repositories {
-        mavenCentral()
-    }
+repositories {
+    mavenCentral()
 }
 
-subprojects {
-    apply(plugin = "java")
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
 
-    java {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
-    }
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+}
 
-    dependencies {
-        testImplementation(platform("org.junit:junit-bom:5.9.1"))
-        testImplementation("org.junit.jupiter:junit-jupiter")
-        testImplementation("org.assertj:assertj-core:3.24.2")
-    }
-
-    tasks.test {
-        useJUnitPlatform()
-    }
+tasks.test {
+    useJUnitPlatform()
 }
