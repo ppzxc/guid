c421775b56d36b4883863d791d2db6fdb73763bd
exit(-1) <ppzxc8487@gmail.com>
2024-03-12 09:18:36 +0900

    9-bug-missing-constructor-in-bucketed-snowflake-guid-generator (#10)
    - BucketedSnowflakeGuidGenerator.java 생성자 수정
    - BucketedSnowflakeGuid.java 내 default bucket size 를 BucketedSnowflakeGuidGenerator.java 로 이동

--- src/main/java/io/github/ppzxc/guid/BucketedSnowflakeGuid.java
+++ src/main/java/io/github/ppzxc/guid/BucketedSnowflakeGuid.java
@@ -5,8 +5,6 @@ package io.github.ppzxc.guid;
  */
 public class BucketedSnowflakeGuid extends SnowflakeGuid {
 
-  private static final long DAY = 1000L * 60L * 60L * 24L;
-  private static final long DEFAULT_BUCKET = DAY * 10;
   private final long bucketSize;
 
   /**
@@ -18,15 +16,6 @@ public class BucketedSnowflakeGuid extends SnowflakeGuid {
   public BucketedSnowflakeGuid(long id, long bucketSize) {
     super(id);
     this.bucketSize = bucketSize;
-  }
-
-  /**
-   * Instantiates a new Bucketed snowflake guid.
-   *
-   * @param id the id
-   */
-  public BucketedSnowflakeGuid(long id) {
-    this(id, DEFAULT_BUCKET);
   }
 
   @Override
